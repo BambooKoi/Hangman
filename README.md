4ed82c67f26f7b7a4179850f3a944c67683d7283
BambooKoi <barberry.koi@gmail.com>
2020-08-18 14:38:34 -0400

    Added ability to edit word list, help menu, changelog. Updated README

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Hangman
-Why not play a game (or more) of Hangman? Made with Python.
+Why not play a game (or more) of Hangman? Made with Python. Play using the default word list of animals or add your own.
 
-![Hangman start screen](demo_images/start_screen.png)
+![Hangman start screen](demo_images/start_screen.gif)
 
 ## Table of Contents
 - [Gameplay](#Gameplay)
@@ -10,6 +10,7 @@ Why not play a game (or more) of Hangman? Made with Python.
     - [Game Over](#Game-Over)
     - [Solve For Me/I Give Up](#Solve-For-Me/I-Give-Up)
     - [Not A Letter](#Not-A-Letter)
+    - [Editing](#Editing)
 - [Built With](#Built-With)
 - [Running the Game](#Running-the-Game)
 - [License](#License)
@@ -17,25 +18,29 @@ Why not play a game (or more) of Hangman? Made with Python.
 - [Donations (Optional)](#Donations-(Optional))
 
 ## Gameplay
-You can play this game endlessly. For those who love CAPS, input is case-insensitive but will output as lowercase. See [Not A Letter](#Not-A-Letter) for invalid inputs. All words in the list are currently animals.
+You can play this game endlessly. For those who love CAPS, input is case-insensitive but will output as lowercase. See [Not A Letter](#Not-A-Letter) for what happens with invalid inputs. All words in the list are animals but fully customizable by the user, see [Editing](#Editing) to learn how.
 
 ### Rules
-For those who have never played Hangman, the game requires two players. In this case, the computer is the second player. One player thinks of a word (in this case, the computer), the other player (you) will try to guess the word by guessing the letters in it. For every wrong guess, a picture of a hanged stick man will be revealed until the stick man is hanged. Then it is game over. You win by correctly guessing the thought up word.
+For those who have never played Hangman, the game requires two players. In this case, the computer is player number two. One player thinks of a secret word (in this case, the computer) while the other player (you) will try and guess the word by guessing the letters in it.
+
+For every incorrect guess, a picture of a hanged stick man will be revealed. When the stick man is hanged, the game is over. To win, correctly guess the secret word and save the stick man from being hanged.
+
+Type and enter `help` any time in the game to pull up the help menu.
 
 ### Winning
 Here's a typical gameplay scenario where the player wins. The player wins when they correctly guess all the letters in whatever word the computer chooses.
 
 ![Hangman gameplay](demo_images/win.gif)
 
-Of course, you can play infinitely until you get bored. Each new word is chosen randomly from a list (inside the Python file).
+Of course, you can play infinitely until you get bored without having to re-launch the game by typing `y` for "yes" at the end to trigger a new game. Each new word is chosen randomly from a list inside the Python file but you can make your own seperate custom word list (covered in [Editing](#Editing)).
 
 ### Game Over
-This is what Game Over looks like. Just like the [winning scenario](#Winning), you can play again as many times as you want afterwards.
+This is what a Game Over scenario looks like. The ASCII stick man updates each time the player makes an incorrect guess. Just like the [winning scenario](#Winning), you can play again as many times as you want afterwards.
 
-![Hangman game over screen](demo_images/lose.png)  #make gif blinking
+![Hangman game over screen](demo_images/lose.gif)
 
 ### Solve For Me/I Give Up
-Can't figure out the word? Get the answer by with `solve` instead of bashing each letter until you die.
+Can't figure out the word? Get the answer by with `solve` instead of bashing each letter until you die or to end the game early and access the [Editing Menu](#Editing).
 
 ![Hangman solve/give up](demo_images/solve.gif)
 
@@ -44,22 +49,70 @@ What happens when you try entering more than one character that's not a letter? 
 
 ![Hangman not a letter prompt](demo_images/notaletter.gif)
 
-The good news is that UPPERCASE and lowercase of the same letter count as the same guess meaning that you won't be penalized for guessing the same letter by accident. Or accidentally inputing a number or special character like "!".
+The good news is that UPPERCASE and lowercase of the same letter count as the same guess meaning that you won't be penalized for guessing the same letter more than once by accident. Or accidentally inputing a number or special character like "!".
 
 Trying to guess more than one character at a time is cheating!
 
+### Editing
+You can call `edit` before and after a new game to add or remove words from the current list. This cannot be done while you are playing Hangman.
+
+In Editing, you can add and remove words, peek at the full word list and save a custom word list. The default word list can be found within the `hangman.py` file itself. It is a list of 65 different animals. If `hangman_words.txt` was not found or couldn't load properly, Hangman will continue to run using the default word list.
+
+#### Adding Words
+You can add as many words as you like so long as they do NOT contain:
+- spaces
+- symbols
+- numbers or
+- special characters.
+Words must be added one at a time and have a minimum of two characters.
+
+![Adding and peeking at words](demo_images/adding_words.png)
+
+In the above example, the words "cookie" and "pie" were added, they are shown at the bottom of the word list in the final word list peek.
+
+Your new word list will apply for the current game session, make sure to save it in the Editing Menu or when prompted at quitting for future sessions.
+
+#### Removing Words
+Don't like the default word list? You can remove any word you don't like from it.
+
+In fact, you can "soft-delete" the entire default list and re-build a list from scratch if you wanted to. "Soft-delete" means that `hangman.py` will still re-use the default list upon relaunching unless you choose an empty `hangman_words.txt` to override it. In this case, you can delete the word-less `hangman_words.txt` file or use the [Editing Menu](#Editing) to add new words.
+
+Your new word list will apply for the current game session, make sure to save it in the Editing Menu or when prompted at quitting for future sessions.
+
+#### Peeking At Current Word List
+Here are the 65 default animal names. Note: Newly added words will be added to bottom of the list. See [Adding Words](#Adding-Words) for adding new words.
+
+![Peek at full word list](demo_images/peek.png)
+
+#### Saving Words
+Players will have the option to save the list from the Editing Menu and will be asked if they want to save again upon quitting.
+
+When saving, the game will try to save a `hangman_words.txt` in the same location as the `hangman.py` file. While it is possible to check and edit the words in `hangman_words.txt` in a .txt reader, it isn't recommended. Opening `hangman_words.txt` won't cause any issues but it is written with special characters that make it possible for Python to import it back into the game if you're thinking about editing the list. See [Adding Words](#Adding-Words) and [Removing Words](#Removing-Words) to edit the word list.
+
+Disclaimer: If you are sharing and using custom `hangman_words.txt` files from others, please ensure that it is from a trusted or authenticated source before downloading and running it.
+
 ## Built With
-- [Python 3.8](python.org)
-    - Using the built-in modules
+- [Python 3.8](python.org) with the help of the built-in modules:
+    - os
+    - random
+    - pickle
+    - sys
 
 ## Running the Game
-- Install Python 3.6 or newer. You can grab the latest copy from [python.org].
+- Install Python 3.6 or newer.
+    - You can grab the latest copy from http://python.org.
 - Download the `hangman.py`.
-- Run `hangman.py` and enjoy guessing the word.
+- Run `hangman.py` and enjoy.
 
-## License
-![GitHub](https://img.shields.io/github/license/BambooKoi/Hangman)
+## Changelog
+August 17, 2020:
+- Added this Changelog
+- Changed list name `animals` to `words` in Python file
+- Added ability to add, remove, see the full word list and save new words locally
+- Added a help screen
+- Updated README images of gameplay and features to reflect new changes
 
+## License ![GitHub](https://img.shields.io/github/license/BambooKoi/Hangman)
 Distributed under the MIT License. See `LICENSE` for more information.
 
 ## Acknowledgements
--- hangman.py
+++ hangman.py
@@ -1,12 +1,14 @@
-# Play hangman
+# Play Hangman
 
 import os
 import random
+import pickle
 import sys
 
 
 def start():
-        print('''
+    os.system('cls')
+    print('''
     +---+
     O   |
    /|\  |
@@ -14,24 +16,163 @@ def start():
        ===
 H _ N G M _ N
             ''')
-        print('Would you like to play Hangman?\n')
+    print('Would you like to play Hangman?\n')
 
 
 def ask():
     choice = ''
     while choice.lower() != 'n':
-        choice = input('(Y)es or (N)o.\n> ')
-        if choice.lower() == 'y':
+        choice = input('(Y)es, (N)o, (EDIT) Word List, (HELP) or (QUIT).\n> ')
+        choice = choice.lower()
+        if choice == 'y':
             break
-        elif choice.lower() == 'n':
+        elif choice == 'n':
+            close()
+        elif choice == 'help':
+            help()
+            input('\nPress (ENTER) to continue. ')
+            start()
+        elif choice == 'edit':
+            edit()
+        elif choice == 'quit':
             close()
         else:
             print(f'\nSorry, I didn\'t understand "{choice}".')
+            print('Please enter one of the options provided in brackets:\n')
     os.system('cls')
 
+
+def help():
+    os.system('cls')
+    print('\n### HELP/RULES ###')
+    print('\nHangman is a game where you try to guess the letters in a secret word.')
+    print('For every incorrect guess, a picture of a hanged stick man will be revealed.')
+    print('When the stick man is hanged, the game is over.')
+    print('To win, correctly guess the secret word and save the stick man from being hanged.')
+
+    print('\nThis game is case-insensitive, so feel free to use all caps or lowercase.')
+    print('Follow the letters or words in brackets for commands to continue.')
+
+    print('\n### ADDING/REMOVING WORDS ###')
+    print('\nYou can add or remove words before and after a new game using (EDIT).')
+    print('From there you have the option of (A)dd, (R)emove or (P)eek at all the words.')
+    print('Saving the word list is also available in this menu and when quitting.')
+    print('New words will appear at the bottom of the list but chosen at random when playing.')
+
+    print('\n### OTHER COMMANDS ###')
+    print('\nYou can quit anytime with (QUIT)')
+    print('And summon this help screen with (HELP).')
+
+
+def edit():
+    os.system('cls')
+    print('### EDITING THE WORD LIST ###')
+    print('\nWhat would you like to do?\n')
+    print('(A)dd a new word to the list.')
+    print('(R)emove an existing word.')
+    print('(P)eek to see all current words.')
+    print('(B)ack for previous menu')
+    print('(S)ave word list now without quitting.')
+    print('\n(HELP) for help or (QUIT) to exit.')
+    choice = input('\n> ')
+    choice = choice.lower()
+    while choice != 'b':
+        if choice == 'b':
+            break
+        # Add New Words
+        if choice == 'a':
+            print('### ADDING NEW WORDS ###')
+            print('\n(B)ack to stop adding new words.')
+            adding = input('What word would you like to add?\n> ')
+            adding = adding.lower()
+            if adding == 'b':
+                break
+            elif adding in words:
+                words.append(adding)
+                print(f'{adding} already exists in the list.')
+            elif not adding.isalpha():
+                print('Letters only please.')
+            elif len(adding) < 1:
+                print('That\'s not a word! Minimum requirement: Two characters.')
+            elif adding not in words:
+                words.append(adding)
+                print(f'"{adding}" was added to the word list.\n')
+        # Remove Existing Words
+        elif choice == 'r':
+            print('\n### REMOVING EXISTING WORDS ###')
+            print('\n(B)ack to stop removing words. (CLEAR ALL) to remove all words.')
+            removing = input('What word would you like to remove?\n> ')
+            if removing.lower() == 'b':
+                break
+            elif removing.lower() in words:
+                words.remove(removing)
+                print(f'"{removing.lower()}" has been removed from the word list.')
+            elif removing.lower() == 'clear all':
+                print('\nAre you sure you want to remove all the words in the list?')
+                print('This will include the default word list and any newly added words.')
+                print('Changes will be applied during this game session.')
+                print('To apply changes for the next session, save at the Editing Menu or Quit Menu.')
+                delete = input('\nRemove all words? Type (YES) to confirm or press (ENTER) to cancel.\n> ')
+                if delete.lower() == 'yes':
+                    words.clear()
+                    print('All words in the list have been removed.')
+                else:
+                    print('No changes have taken place.')
+            else:
+                print(f'"{removing}" doesn\'t exist in the word list. Check your spelling.')
+        # See Full List of Current Words
+        elif choice == 'p':
+            # Print 10 words per line:
+            print('\n### YOUR CURRENT WORD LIST: ###\n')
+            for word in range(0, len(words), 10):
+                print('\t'.join(words[word:word + 10]))
+            input('\nPress (ENTER) to go to the Starting Screen. ')
+            break
+        elif choice == 's':
+            save()
+            input('\nPress (ENTER) to continue. ')
+            break
+        elif choice == 'help':
+            help()
+            input('\nPress (ENTER) to continue. ')
+            break
+        elif choice == 'quit':
+            close()
+        else:
+            input(f'\nSorry, I did not understand "{choice}". (ENTER) to continue. ')
+            break
+    os.system('cls')
+    start()
+            
+
+def save():
+    try:
+        with open('hangman_words.txt', 'wb') as f:
+            pickle.dump(words, f)
+            print('\nWord list saved.')
+    except Exception as e:
+        print(e)
+        print('\nSorry, I couldn\'t figure out how to save your words.')
+
+
 def close():
-    input('\nThanks for playing! Hit enter to exit.')
+    print('\nWould you like to save any newly added words?')
+    saving = input('Enter (Y)es, (N)o or (HELP).\n> ')
+    saving = saving.lower()
+    if saving == 'y':
+       save()
+    elif saving == 'n':
+        pass
+    elif saving == 'help':
+        help()
+        # Loop back to whether the user would like to save.
+        close()
+    else:
+        print(f'\nSorry I do not understand "{saving}".')
+
+    input('\nThanks for playing! Hit (ENTER) to exit.')
     sys.exit()
+
 
 def playing():
     while True:
@@ -48,7 +189,7 @@ def playing():
             guess = input('This is your LAST guess.\n> ')
         else:
             print('Enter a letter to guess.')
-            guess = input('Use "solve" to give up or "quit" to exit.\n> ')
+            guess = input('(SOLVE) for the solution, (HELP) for help, or (QUIT) to exit.\n> ')
             guess = guess.lower()
 
         if guess == 'solve':
@@ -58,11 +199,14 @@ def playing():
             print(' '.join(blanks))
             print(f'\nThe word was:', ''.join(answer))
             break
+        elif guess == 'help':
+            help()
+            input('\nPress (ENTER) to continue. ')
         elif guess == 'quit':
             close()
         elif len(guess) > 1:
             msg.append('That\'s too many characters! One at a time please.')
-        elif guess not in 'abcdefghijklmnopqrstuvwxyz':
+        elif not guess.isalpha():
             promp = f'"{guess}" is not a letter.'
             msg.append(promp)
         elif (guess in guesses) or (guess in blanks):
@@ -138,7 +282,7 @@ hangman = ["""
    / \  |
        ==="""]
 
-animals = [
+words = [
     'ant', 'baboon', 'badger', 'bat', 'bear', 'beaver', 'camel', 'cat',
     'clam', 'cobra', 'cougar', 'coyote', 'crow', 'deer', 'dog', 'donkey',
     'duck', 'eagle', 'ferret', 'fox', 'frog', 'goat', 'goose', 'hawk', 'lion',
@@ -154,11 +298,33 @@ answer = []
 blanks = []
 msg = []
 
+# Check if an existing word list exists.
+# If yes, update the default list.
+# Else, use the default list.
+try:
+    with open('hangman_words.txt', 'rb') as f:
+        print('A previously saved word list was found, would you like to use it?')
+        custom = input('(Y)es or (N)o.\n> ')
+        if custom.lower() == 'y':
+            words = pickle.load(f)
+            input('Saved word list loaded. Press (ENTER) to continue.\n')
+        elif custom.lower() == 'n':
+            input('Using default word list. Press (ENTER) to continue.\n')
+except:
+    print('Playing with default word list.\n')
+
+# Playing Hangman:
 start()
 while True:
     ask()
-    answer = list(random.choice(animals))
-    # OR answer = [letter for letter in random.choice(animals)]
+    if len(words) == 0:
+        print('There are no words in the list to choose from.')
+        input('Press (ENTER) to go to the Editing Menu. ')
+        edit()
+    else:
+        pass
+    answer = list(random.choice(words))
+    # OR answer = [letter for letter in random.choice(words)]
     blanks = ['_' for letter in answer]
     msg.clear()
     playing()
